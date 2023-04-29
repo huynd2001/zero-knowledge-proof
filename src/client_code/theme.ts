d7c1f4b215aba3c0c95308ab6a744b9e8adfa746
Duc Huy Nguyen <huynd2001@Ducs-MacBook-Pro-2.local>
2023-04-29 17:57:12 -0400

    feat: add material ui

---
+++ src/client_code/theme.ts
@@ -0,0 +1,8 @@
+// src/theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // Add your custom theme configurations here
+});
+
+export default theme;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../client_code/createEmotionCache";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "../client_code/theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props: any) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
